1cf40440cdcf1318258092297e48c9c4c88d79eb
Allan Leal <allanjohnson.leal@gmail.com>
2021-05-04 21:27:41 -0300

    remove/deleted register and sigin for the sake of not needing   created new design for better visualization

--- src/app.js
+++ src/app.js
@@ -4,13 +4,10 @@ const app = express()
 const navigationController = require('./controllers/NavigationController');
 const categoriesController = require('./controllers/CategoriesController');
 const articlesController = require('./controllers/ArticlesController');
-const registerController = require('./controllers/RegisterController');
-const signinController = require('./controllers/SigninController')
 
 
 const articleModel = require('./models/Article');
 const categoryModel = require('./models/Category');
-const registerModel = require('./models/Register');
 
 
 app.use(express.static(path.join(__dirname+ '/..', 'public')))
@@ -22,8 +19,6 @@ app.set('view engine', 'ejs')
 app.use('/', navigationController);
 app.use('/', categoriesController);
 app.use('/', articlesController);
-app.use('/', registerController);
-app.use('/', signinController)
 
 
 

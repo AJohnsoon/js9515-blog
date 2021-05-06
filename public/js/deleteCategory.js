3ffd2f9a9db3193ad77f5930a13e967a9408c171
Allan Leal <allanjohnson.leal@gmail.com>
2021-05-06 18:51:09 -0300

    feature: add security to delete a category

---
+++ public/js/deleteCategory.js
@@ -0,0 +1,5 @@
+function deleteCategory(event, form){
+    event.preventDefault();
+    let decision = confirm("Deseja realmente deletar essa categoria?")
+    decision ? form.submit(alert('Category has been deleted')) : console.log('user canceled delete operation ')
+}

002aeac6cce65a7fd8a5fad2d37a7d36aa51c00e
Allan Leal <allanjohnson.leal@gmail.com>
2021-05-06 00:34:36 -0300

    feat: add detele router and functionality categories in database

--- src/controllers/CategoriesController.js
+++ src/controllers/CategoriesController.js
@@ -3,28 +3,54 @@ const router = express.Router();
 const modelCategory = require('../models/Category')
 const slugify = require('slugify')
 
-router.get('/admin/categories/new', (req, res)=>{    
+router.get('/admin/categories/new', (req, res) => {
     res.render('views/admin/category', {})
 })
 
-router.post('/categories/save', (req, res)=>{
+router.post('/categories/save', (req, res) => {
     let title = req.body.title;
-    if( title === undefined || title === '' || title === null){
-        res.redirect("/admin/categories/new")                
-    }else{
+    if (title === undefined || title === '' || title === null) {
+        res.redirect("/admin/categories/new")
+    } else {
         modelCategory.create({
             title: title,
             slug: slugify(title)
-        }).then(()=>{
+        }).then(() => {
             res.redirect('/admin/categories/')
         })
     }
 })
 
-router.get('/admin/categories/', (req, res)=>{
-    modelCategory.findAll().then(( categories)=>{
-        res.render('views/admin/index', {showCategories: categories})
+router.get('/admin/categories/', (req, res) => {
+    modelCategory.findAll().then((categories) => {
+        res.render('views/admin/index', {
+            showCategories: categories
+        })
     })
 })
+
+
+router.post("/categories/delete", (req, res) => {
+    let id = req.body.id;
+    if(id != undefined){
+        if(!isNaN(id)){
+            modelCategory.destroy({
+                where: {
+                    id:id
+                }
+            }).then(()=>{
+                res.redirect('/admin/categories')
+            })        
+        }
+        else{
+            res.redirect('/')
+        }    
+    }
+    else{
+        res.redirect('/')
+    }
+
+})
+
 
 module.exports = router

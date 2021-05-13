fce74064f7845e26274f1df35fd8736238e5e3c4
Allan Leal <allanjohnson.leal@gmail.com>
2021-05-12 21:37:05 -0300

    refactor: added comment from brazil timezone

--- src/database/data.js
+++ src/database/data.js
@@ -2,6 +2,7 @@ const Sequelize = require('sequelize');
 const connection = new Sequelize('js9515_blog', 'root', '', {
     host: 'localhost',
     dialect: 'mysql'
+    //timezone: '-03:00'
 });
 
 module.exports = connection

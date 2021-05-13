32a7041705e16bdafd56127f6123efb3f572a7a7
Allan Leal <allanjohnson.leal@gmail.com>
2021-05-12 21:39:20 -0300

    add tinymce to the project

---
+++ public/js/tiny.js
@@ -0,0 +1,6 @@
+tinymce.init({
+    selector: "#article-body",
+    plugins: [
+        'advlist autolink link image lists print preview hr searchreplace wordcount insertdatetime paste emoji', 
+    ]
+})

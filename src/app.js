6dded8c1a0576794af1a84977077a98642426ee5
Allan Leal <allanjohnson.leal@gmail.com>
2021-06-11 22:28:53 -0300

    feat: add and config express-session

--- src/app.js
+++ src/app.js
@@ -1,17 +1,25 @@
-const express = require('express')
+const express = require('express');
+const session = require('express-session');
 const path = require('path');
-const app = express()
+const app = express();
 const navigationController = require('./controllers/NavigationController');
 const categoriesController = require('./controllers/CategoriesController');
 const articlesController = require('./controllers/ArticlesController');
 const publicationController = require('./controllers/PublicationController');
 const userController = require('./controllers/UserController');
 
-app.use(express.static(path.join(__dirname+ '/..', 'public')))
-app.set('views', path.join(__dirname+ '/..', 'public'))
-app.use(express.urlencoded({extended: false}))
-app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname+ '/..', 'public')));
+app.set('views', path.join(__dirname+ '/..', 'public'));
+app.use(express.urlencoded({extended: false}));
+app.set('view engine', 'ejs');
 
+app.use(session({
+    secret: 'keyboard word',
+    cookie: {
+        secure: true,
+        maxAge: 30000
+    }
+}))
 
 app.use('/', navigationController);
 app.use('/', categoriesController);
@@ -23,4 +31,4 @@ app.use('/', userController);
 
 
 
-module.exports = app
+module.exports = app;

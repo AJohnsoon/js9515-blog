c641eacd5b023db30bcf5bb09a9cdf197f0a8c3d
Allan Leal <allanjohnson.leal@gmail.com>
2021-06-14 15:33:44 -0300

    feat: add session lib

--- src/app.js
+++ src/app.js
@@ -1,26 +1,36 @@
-const express = require('express')
+const express = require('express');
+const session = require('express-session');
 const path = require('path');
-const app = express()
+const app = express();
 const navigationController = require('./controllers/NavigationController');
 const categoriesController = require('./controllers/CategoriesController');
 const articlesController = require('./controllers/ArticlesController');
 const publicationController = require('./controllers/PublicationController');
 const userController = require('./controllers/UserController');
 
-app.use(express.static(path.join(__dirname+ '/..', 'public')))
-app.set('views', path.join(__dirname+ '/..', 'public'))
-app.use(express.urlencoded({extended: false}))
-app.set('view engine', 'ejs')
+const sessionController = require('./controllers/sessionController');
 
+app.use(express.static(path.join(__dirname+ '/..', 'public')));
+app.set('views', path.join(__dirname+ '/..', 'public'));
+app.use(express.urlencoded({extended: false}));
+app.set('view engine', 'ejs');
+
+app.use(session({
+    secret: 'keyboard word',
+    cookie: {
+        secure: true,
+        maxAge: 30000
+    }
+}))
 
 app.use('/', navigationController);
 app.use('/', categoriesController);
 app.use('/', articlesController);
 app.use('/', publicationController);
 app.use('/', userController);
+app.use('/', sessionController);
 
 
 
 
-
-module.exports = app
+module.exports = app;
